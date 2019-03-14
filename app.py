03fdef9f9fcb168fb21662f842be58fe97f05a89
Guy Royse <guy@guyroyse.com>
2019-03-14 16:37:37 -0400

    commented and single file

--- app.py
+++ app.py
@@ -5,11 +5,14 @@ from flask_cors import CORS, cross_origin
 
 from settings import Settings
 
+# create the flask application
 app = Flask(__name__)
 
+# get the DataRobot AI client and AI learning session
 dr = DataRobotAIClient.create(username=Settings.USERNAME, key=Settings.API_KEY)
 ai = dr.projects.get_project(Settings.PROJECT_ID)
 
+# /info route returns information about the application
 @app.route('/info', methods=['GET'])
 @cross_origin()
 def info():
@@ -19,24 +22,34 @@ def info():
     'attribution' : Settings.APP_ATTRIBUTION
   })
 
+# /classinate route classinates a Bigfoot sighting
 @app.route('/classinate', methods=['POST'])
 @cross_origin()
 def classinate():
 
-  sighting = request.get_json()['sighting']
+  # get the sighting from the request
+  request_json = request.get_json()
+  sighting = request_json['sighting']
 
-  predictions = ai.infer('classification', [{ 'observed': sighting }])
-  p = predictions[0]
+  # setup the prediction data
+  # NOTE: ai.infer expects an array and can make multiple predictions
+  # at once, but we only need one so we wrap it in an array
+  data = [{ 'observed': sighting }]
 
+  # infer the predictions and select the first one
+  prediction = ai.infer('classification', data)[0]
+
+  # return the prediction as JSON in the expected format
   return jsonify({
     'sighting' : sighting,
     'classination' : {
-      'class_a'  : p.score_for("Class A"),
-      'class_b'  : p.score_for("Class B"),
-      'class_c'  : p.score_for("Class C"),
-      'selected' : p.prediction
+      'class_a'  : prediction.score_for("Class A"),
+      'class_b'  : prediction.score_for("Class B"),
+      'class_c'  : prediction.score_for("Class C"),
+      'selected' : prediction.prediction
     }
   })
 
+# kick off the flask
 if __name__ == '__main__':
   app.run()
