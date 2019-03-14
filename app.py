0eae0121e30f122a9982520d78d904b1fae9e176
Guy Royse <guy@guyroyse.com>
2019-03-14 16:29:20 -0400

    paring it down

--- app.py
+++ app.py
@@ -1,46 +1,42 @@
-import sys
+from datarobotai.client import DataRobotAIClient
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 
-from classinator import BigfootClassinatorModel
 from settings import Settings
 
 app = Flask(__name__)
 
+dr = DataRobotAIClient.create(username=Settings.USERNAME, key=Settings.API_KEY)
+ai = dr.projects.get_project(Settings.PROJECT_ID)
 
 @app.route('/info', methods=['GET'])
 @cross_origin()
 def info():
-
-  response = jsonify({
+  return jsonify({
     'app'     : Settings.APP_NAME,
     'version' : Settings.APP_VERSION,
     'attribution' : Settings.APP_ATTRIBUTION
   })
 
-  print(f"Response: {response.get_json()}")
-
-  return response
-
 @app.route('/classinate', methods=['POST'])
 @cross_origin()
 def classinate():
 
-  print(f"Request: {request.get_json()}")
-
   sighting = request.get_json()['sighting']
 
-  print(sighting)
+  predictions = ai.infer('classification', [{ 'observed': sighting }])
+  p = predictions[0]
 
-  model = BigfootClassinatorModel()
-  classination = model.classinate(sighting)
-
-  response = jsonify(classination)
-
-  print(f"Response: {response.get_json()}")
-
-  return response
+  return jsonify({
+    'sighting' : sighting,
+    'classination' : {
+      'class_a'  : p.score_for("Class A"),
+      'class_b'  : p.score_for("Class B"),
+      'class_c'  : p.score_for("Class C"),
+      'selected' : p.prediction
+    }
+  })
 
 if __name__ == '__main__':
   app.run()
