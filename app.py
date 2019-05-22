27c0420eb0e90e454ec7cfda339c003d760a9823
Guy Royse <guy@guyroyse.com>
2019-05-22 13:17:59 -0400

    including coords in request, checks for believability but does nothing with it yet

--- app.py
+++ app.py
@@ -16,12 +16,12 @@ project_id = os.environ['BIGFOOT_CLASSINATOR_PROJECT_ID']
 dr = DataRobotAIClient.create(api_key)
 
 # load the project
-ai = dr.projects.get_project(project_id)
+ai = dr.projects.get(project_id)
 
 # /info route returns information about the application
 @app.route('/info', methods=['GET'])
 @cross_origin()
-def info():
+def info_route():
   return jsonify({
     'app'         : "Bigfoot Classinator",
     'version'     : "3.0.1",
@@ -31,30 +31,58 @@ def info():
 # /classinate route classinates a Bigfoot sighting
 @app.route('/classinate', methods=['POST'])
 @cross_origin()
-def classinate():
+def classinate_route():
 
-  # get the sighting from the request
+  # get the sighting info from the request
   request_json = request.get_json()
+  latitude = request_json['latitude']
+  longitude = request_json['longitude']
   sighting = request_json['sighting']
 
-  # setup the prediction data
-  # NOTE: ai.infer expects an array and can make multiple predictions
-  # at once, but we only need one so we wrap it in an array
-  data = [{ 'observed': sighting }]
+  # prediction the class and then use it to predict the believability
+  classination, class_a, class_b = classinate(latitude, longitude, sighting)
+  believability = believify(sighting, classination)
 
-  # infer the predictions and select the first one
-  prediction = ai.infer('classification', data)[0]
+  # if the believability is high enough, store it
+  if believability >= 3.0:
+    store_sighting(latitude, longitude, sighting, classination)
 
   # return the prediction as JSON in the expected format
   return jsonify({
     'sighting' : sighting,
     'classination' : {
-      'class_a'  : prediction.score_for("Class A"),
-      'class_b'  : prediction.score_for("Class B"),
-      'class_c'  : prediction.score_for("Class C"),
-      'selected' : prediction.prediction
+      'class_a'  : class_a,
+      'class_b'  : class_b,
+      'selected' : classination
     }
   })
+
+def classinate(latitude, longitude, sighting):
+
+  # setup the prediction data
+  data = [{ 'latitude': latitude, 'longitude': longitude, 'observed': sighting }]
+
+  # infer the predictions and select the first one
+  prediction = list(ai.infer('classification', data))[0]
+
+  # return the class and scores
+  return prediction.prediction, prediction.score_for("Class A"), prediction.score_for("Class B")
+
+def believify(sighting, classification):
+
+  # setup the prediction data
+  data = [{ 'observed': sighting, 'classification': classification }]
+
+  # infer the predictions and select the first one
+  prediction = list(ai.infer('believability', data))[0]
+
+  # return the believability
+  return prediction.prediction
+
+def store_sighting(latitude, longitude, sighting, classination):
+  # store it somehow
+  return
+
 
 # kick off the flask
 if __name__ == '__main__':
